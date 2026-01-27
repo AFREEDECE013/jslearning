d46d671f5f10ff9086b4355cdd2466cfdb6bd268
AFREEDECE013 <mogalafreed2000@gmail.com>
2026-01-27 18:40:06 +0530

    added datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,18 @@
+// Primitive
+// 7 types: string, number, bool, null, undefined, bigint, symbol--> which is a unique value
+
+const bigNumber = 90852932945830983298490n
+const ID = Symbol('123')
+
+// Reference (non-premitive)
+// array, objcts, functions
+
+const heros = ["Ironman", "cap", "thor"]
+let myObj = {
+    name : "Afreed",
+    age : "25"
+}
+
+const myFunction = function() {
+    console.log("Hello World!!!")
+}
