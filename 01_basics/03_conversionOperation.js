ebc8415314c7eb42aab108a13b3d4a5ef06b4276
AFREEDECE013 <mogalafreed2000@gmail.com>
2026-01-27 17:10:33 +0530

    3 updated

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -55,3 +55,44 @@ console.log("Original Type", typeof(valueInString))
 let updateValueInString = String(valueInString)
 
 console.log("Updated Type", typeof(updateValueInString))
+
+
+// *********************************** Operations **************************************
+
+let opValue1 = 3
+let negOpValue1 = -opValue1
+console.log(negOpValue1);
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/3)
+console.log(2%2)
+
+//
+
+let str1 = "Hello"
+let str2 = " Afreed"
+
+console.log(str1 + str2);
+
+
+//
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+console.log(+true)
+console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 1
+
+let gameCounter = 100
+++gameCounter
+
+console.log(gameCounter);
