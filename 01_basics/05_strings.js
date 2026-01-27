3ce3ea620dcb41aa4cdfc8dcb6d0632d1ebd20f7
AFREEDECE013 <mogalafreed2000@gmail.com>
2026-01-27 19:41:48 +0530

    05 updated

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const name = "Afreed"
+const age = 25
+
+console.log(`Hi my name is ${name} and my age is ${age}`)
+console.log(name.length)
+
+const gameName = new String("afreed-mb")
+console.log(gameName[0])
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf("f"))
+
+
+const newString = gameName.substring(0, 4)
+console.log(newString)
+
+const anotherString = gameName.slice(-7, 4)
+console.log(anotherString)
+
+const newStringOne = " Afreed     "
+console.log(newStringOne.trim())
+
+const url = "https://test.com/afreed%20mb"
+console.log(url.replace("%20", "-"))
+console.log(url.includes("test"))
+
+test_split = gameName.split("-")
+console.log(test_split)
--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,6 +1,8 @@
 // Primitive
 // 7 types: string, number, bool, null, undefined, bigint, symbol--> which is a unique value
 
+const { use } = require("react")
+
 const bigNumber = 90852932945830983298490n
 const ID = Symbol('123')
 
@@ -16,3 +18,33 @@ let myObj = {
 const myFunction = function() {
     console.log("Hello World!!!")
 }
+
+
+// -------------------------------------------------------------
+
+// Stack (Primitive), Heap (Non-primitive)
+// Stack --> we will get a copy
+// heap --> Reference (it will update the original value)
+
+let original = "hi"
+
+let duplicate = original
+
+duplicate = "Hello"
+
+console.log(original)
+console.log(duplicate);
+
+// 
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl" 
+}
+
+let userTwo = userOne
+
+userTwo.email = "afreed@google"
+
+console.log(userOne.email)
+console.log(userTwo.email)
