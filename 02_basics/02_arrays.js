1f1ae3111cdad9d93fc1e494043deb21ea0614cc
AFREEDECE013 <mogalafreed2000@gmail.com>
2026-01-28 13:36:41 +0530

    updated  in 2 01 and 02 is added

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,28 @@
+const marvel_heros = ["ironman", "Thor", "Hulk"]
+const dc_heros = ["superman", "batman", "flash"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+
+// console.log(allHeros)
+
+// const my_all_heros = [...dc_heros, ...marvel_heros] //spread operator
+// console.log(my_all_heros)
+
+// const anotherArray = [1,2,3,[4,5], 6, [7,[8,9]]]
+// const another_real_array = anotherArray.flat(Infinity)
+// console.log(another_real_array);
+
+
+// console.log(Array.isArray(dc_heros))
+// console.log(Array.from("Afreed"))
+// console.log(Array.from({name: "Afreed"}))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3))
